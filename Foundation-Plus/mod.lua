2d133b9157d44df33ba89ecdc4b18f24dc8c5e70
VMPYRC <90119648+VMPYRC@users.noreply.github.com>
2025-03-26 11:33:49 -0400

    new game start

--- Foundation-Plus/mod.lua
+++ Foundation-Plus/mod.lua
@@ -1,1 +1,10 @@
 local foundationPlus = foundation.createMod()
+
+-- game start --
+
+foundationPlus:overrideAsset({
+	Id = "DEFAULT_BALANCING",
+	InitialFamilyCount = 50,
+	InitialGoldCapacity = 100000,
+	MinimumHappinessForLeaving = 1,
+})
