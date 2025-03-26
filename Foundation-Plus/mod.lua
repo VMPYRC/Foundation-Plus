bf129208eb33e20ca80de5db1c4a28a95b55d2b5
VMPYRC <90119648+VMPYRC@users.noreply.github.com>
2025-03-26 11:34:56 -0400

    immigration

--- Foundation-Plus/mod.lua
+++ Foundation-Plus/mod.lua
@@ -1,6 +1,6 @@
 local foundationPlus = foundation.createMod()
 
--- game start --
+-- new game start --
 
 foundationPlus:overrideAsset({
 	Id = "DEFAULT_BALANCING",
@@ -8,3 +8,39 @@ foundationPlus:overrideAsset({
 	InitialGoldCapacity = 100000,
 	MinimumHappinessForLeaving = 1,
 })
+
+-- immigration --
+
+foundationPlus:overrideAsset({
+	Id = "DEFAULT_BALANCING",
+	ImmigrationFactorList = {},
+})
+
+foundationPlus:overrideAsset({
+	Id = "DEFAULT_IMMIGRATION_SETTINGS",
+	ImmigrationWaveDelayInDays = 1,
+	ImmigrationProbabilitySettingList = {
+		MAXIMUM = {
+			ProbabilityUpperValue = 1.0,
+			VillagerCountRange = { 10, 10 },
+		},
+		HIGH = {
+			ProbabilityUpperValue = 0.8,
+			VillagerCountRange = { 8, 8 },
+		},
+		AVERAGE = {
+			ProbabilityUpperValue = 0.6,
+			VillagerCountRange = { 6, 6 },
+		},
+		LOW = {
+			ProbabilityUpperValue = 0.4,
+			VillagerCountRange = { 4, 4 },
+		},
+		NIL = {
+			ProbabilityUpperValue = 0.0,
+			VillagerCountRange = { 0, 0 },
+		},
+	},
+})
+
+-- split --
